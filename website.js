1a5fa617f4b0e8c562c85ce15459a4421b4ab7ab
yyc12345 <yyc12321@outlook.com>
2018-11-10 23:45:29 +0800

    process ugly style

--- website.js
+++ website.js
@@ -1,16 +1,33 @@
-window.addEventListener("load", add_blur);
+window.addEventListener("load", add_bk);
 window.addEventListener("load", add_kawaii);
 window.addEventListener("load", remove_url);
+window.addEventListener("load", remove_theme);
+document.addEventListener("DOMSubtreeModified", remove_url);
+document.addEventListener("DOMSubtreeModified", remove_theme);
 
 //add a blur
-function add_blur() {
+function add_bk() {
     var body = document.body;
     var item = document.createElement("div");
-    var dark = document.createElement("div");
-    item.innerHTML = "<img class=\"img-blur\" src=\"./assets/bg.png\"></img>"
-    dark.classList.add("bg-dark");
+    //var dark = document.createElement("div");
+
+    var date = new Date;
+    var month = date.getMonth() + 1;
+    var season = "";
+    if (month >= 3 && month <= 5) {
+        season = "spring";
+    } else if (month >= 6 && month <= 8) {
+        season = "summer";
+    } else if (month >= 9 && month <= 11) {
+        season = "autumn";
+    } else {
+        season = "winter";
+    }
+    item.style.cssText = "background: url(./assets/bg-" + season + ".jpg) no-repeat center center fixed; background-size: cover;";
+
+    //dark.classList.add("bg-dark");
     item.classList.add("bg-blur");
-    body.prepend(dark);
+    //body.prepend(dark);
     body.prepend(item);
 }
 
@@ -23,6 +40,19 @@ function add_kawaii() {
 }
 
 function remove_url() {
-    var link = document.getElementsByClassName("gitbook-link");
-    link[0].parentNode.removeChild(link[0]);
+    if ($(".gitbook-link").length > 0) {
+        var link = document.getElementsByClassName("gitbook-link");
+        link[0].parentNode.removeChild(link[0]);
+    }
+}
+
+function remove_theme() {
+    if ($(".size-3").length == 3) {
+        var link = document.getElementsByClassName("size-3");
+        link[1].parentNode.removeChild(link[1]);
+        link[0].classList.add("size-2");
+        link[0].classList.remove("size-3");
+        link[0].classList.add("size-2");
+        link[0].classList.remove("size-3");
+    }
 }
