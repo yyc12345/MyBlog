ccaf53a3bace77457e158a59fc2f9065d805773b
yyc12345 <yyc12321@outlook.com>
2021-11-04 14:16:23 +0800

    fix article and add dark mode

--- docs/javascript/website.js
+++ docs/javascript/website.js
@@ -4,8 +4,4 @@ window.addEventListener("load", remove_url);
 function remove_url() {
     var shitNav = document.querySelector('nav.md-footer__inner');
     shitNav.parentNode.removeChild(shitNav);
-
-    var shitCopyright = document.querySelector('div.md-footer-copyright');
-    shitCopyright.innerHTML="Powered by <a href=\"https://www.mkdocs.org\">MkDocs</a> and <a href=\"https://squidfunk.github.io/mkdocs-material/\">Material for MkDocs</a>. Text is available under the <a href=\"https://creativecommons.org/licenses/by-nc-sa/4.0/\">CC-BY-NC-SA 4.0</a>.";
-
 }
